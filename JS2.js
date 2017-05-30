a474ce1037c81fe3b94dc4784e3c8bdd6707118b
chriscaraway <chrisccaraway@gmail.com>
2017-05-29 22:25:24 -0400

    update

---
+++ JS2.js
@@ -0,0 +1,10 @@
+//compliment
+
+function compliment(name)  {
+  return(name + " is great! ");
+}
+
+
+console.log(compliment("Chris"));
+console.log(compliment("Will"));
+console.log(compliment("Sam"));
